97f2b0d40e482fd3e932b2193af4e2521357c3ab
Ethan-23 <emanroberts@gmail.com>
2024-09-13 22:35:13 -0400

    Added some icons for multis

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class BoardManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> boards = new List<GameObject>();
+    GameObject mainCamera;
     PlayerManager player;
     
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = GameObject.Find("Main Camera");
         player = GameObject.Find("Player").GetComponent<PlayerManager>();
+        if(player.GetBoardSize() == 1)
+        {
+            mainCamera.transform.position = new Vector3 (mainCamera.transform.position.x, 1.4f, mainCamera.transform.position.z);
+            mainCamera.GetComponent<Camera>().orthographicSize = 2;
+        }
         Instantiate(boards[player.GetBoardSize() - 1]);
     }
 
